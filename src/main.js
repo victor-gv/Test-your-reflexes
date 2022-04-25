862187ec4c067aaab96911390626cf43dac169c7
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-04-25 11:58:39 +0200

    js bug with function validateUsername solved

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //Dom Elements, 
 const userName = document.getElementById("user_name");
-const errorUser = document.ggetElementById("error_user");
+const errorUser = document.getElementById("error_user");
 const startBtn = document.getElementById("start_btn");
 const rankingBtn = document.getElementById("ranking_btn");
 const startGame = document.getElementById("start_game");
@@ -8,19 +8,17 @@ const stopBtn = document.getElementById("stop_btn");
 const replayBtn = document.getElementById("replay_btn");
 
 // Global Variables
+
 const errorClass = errorUser.classList;
-console.log(errorClass);
-document.getElementById("startBtn").addEventListener("click", validateUsername);
+startBtn.addEventListener("click", validateUsername);
 
-function validateUsername(){
-if(userName.value == "") {
-let result = errorClass.toggle("error");
-if (result == true) {
-errorUser.textContent = "Invalid username";
+function validateUsername() {
+    if (userName.value == "") {
+        let result = errorClass.toggle("error");
+        if (result == true) {
+            errorUser.textContent = "Invalid username";
+        } else {
+            errorUser.textContent = "";
+        }
+    }
 }
-} else {
-
-}
-}
-
-
