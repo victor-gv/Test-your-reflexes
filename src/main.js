f5b0ae9a0b8c9676421d283c394ec257f08387b5
Francisco Javier Fernandez Baña <levelbierzo@gmail.com>
2022-04-25 11:29:20 +0200

    push to StartDev

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-const start_btn
-const user_name
-const ranking_btn
-const start_game
-const stop_btn
-const replay_btn
+//Dom Elements, 
+const userName = document.getElementById("user_name");
+const errorUser = document.ggetElementById("error_user");
+const startBtn = document.getElementById("start_btn");
+const rankingBtn = document.getElementById("ranking_btn");
+const startGame = document.getElementById("start_game");
+const stopBtn = document.getElementById("stop_btn");
+const replayBtn = document.getElementById("replay_btn");
+
+// Global Variables
+const errorClass = errorUser.classList;
+console.log(errorClass);
+document.getElementById("startBtn").addEventListener("click", validateUsername);
+
+function validateUsername(){
+if(userName.value == "") {
+let result = errorClass.toggle("error");
+if (result == true) {
+errorUser.textContent = "Invalid username";
+}
+} else {
+
+}
+}
+
+
